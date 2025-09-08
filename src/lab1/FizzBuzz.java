413367746158e26cbc2026ea03a71be2e94cbc07
parkdongmyeong0707 <white040707@gmail.com>
2025-09-08 11:08:24 -0400

    some init msg

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,8 +6,12 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
+        doFizzBuzz();
+    }
 
-        for (int i = 1; i < 100; i++) {
+    private static void doFizzBuzz() {
+        int i = 1;
+        while(i < 101) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +35,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
