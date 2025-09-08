7112849189662ba839378d5b5cfc2db46c2a3060
parkdongmyeong0707 <white040707@gmail.com>
2025-09-08 19:28:28 -0400

    refactored to extract helper method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,21 @@
 package lab1;
 
-public class Multiple {
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples();
+        System.out.println(count);
+    }
+    public static int multiples(){
+        return multiples(1000, 3, 5);
+    }
+    public static int multiples(int n, int a, int b){
+        int count = 0;
+        for(int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
