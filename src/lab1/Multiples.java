3ecad7933053d7368a22e0e38cd657ff7e896f1e
parkdongmyeong0707 <white040707@gmail.com>
2025-09-09 02:25:02 -0400

    refactored to extract helper method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -11,7 +11,7 @@ public class Multiples {
     }
 
     // Generalized version
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
